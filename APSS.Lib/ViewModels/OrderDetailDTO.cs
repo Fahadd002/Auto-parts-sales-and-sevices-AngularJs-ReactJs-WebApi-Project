9eccf51b434e113f9589090fc312562d2cfcce33
Fahadd002 <fpramanik6@gmail.com>
2024-07-15 03:22:14 +0600

    Auto parts sales and sevices AngularJs ReactJs WebApi Project

---
+++ APSS.Lib/ViewModels/OrderDetailDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APSS.Lib.ViewModels
+{
+    public class OrderDetailDTO
+    {
+        public int OrderDetailId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; } 
+        public int Quantity { get; set; }
+
+    }
+}
